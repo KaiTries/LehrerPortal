60788ed445e0da1b3d02742cb9133880fc837455
jonaslanzlinger <laenzlinger.jonas@gmail.com>
2024-03-21 16:18:20 +0100

    student.html

--- LehrerPortal/src/main/resources/static/js/student.js
+++ LehrerPortal/src/main/resources/static/js/student.js
@@ -1,6 +1,6 @@
 // import { lehrplan_21 } from "./resources/lehrplan-21-kanton-st-gallen";
 
-let currentSelection = "none";
+let currentSelection = null;
 
 document.getElementById("create-lernziel-btn").addEventListener("click", function () {
 
@@ -8,10 +8,10 @@ document.getElementById("create-lernziel-btn").addEventListener("click", functio
     const type = document.getElementById("type").value;
     const lernzielData = {
         uid: "abc123",
-        fb_id: 1,
+        fb_id: currentSelection,
         f_id: 0,
-        kb_id: 789,
-        ha_id: 1011,
+        kb_id: 0,
+        ha_id: 0,
         k_id: "xyz789",
         code: "ABC123",
         aufbau: "Some aufbau",
@@ -33,7 +33,7 @@ document.getElementById("create-lernziel-btn").addEventListener("click", functio
             if (response.ok) {
                 alert("Lernziel created successfully");
                 togglePopup();
-
+                window.location.reload();
             } else {
                 console.error("Failed to create Lernziel");
             }
@@ -86,240 +86,56 @@ const removeAll = () => {
 
 document.getElementById('deutsch-link').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default link behavior
-
-    currentSelection = "deutsch";
-
+    currentSelection = 0;
     removeAll();
     document.getElementById("deutsch").style.display = "block";
-
-    // const elements = lehrplan_21.filter((item) => item.f_id === 0 && item.bezeichnung !== null);
-    //
-    // for (const item of elements) {
-    //     let newElement = document.createElement('div');
-    //     newElement.classList.add('card'); // Adding a class for styling (you can customize this class in your CSS)
-    //
-    //     // Creating elements for subject, category, title, and description
-    //     let strukturtypElement = document.createElement('h2');
-    //     strukturtypElement.textContent = item.strukturtyp;
-    //     let bezeichnungElement = document.createElement('p');
-    //     bezeichnungElement.textContent = `Bezeichnung: ${item.bezeichnung}`;
-    //
-    //     // Appending elements to the newElement
-    //     newElement.appendChild(strukturtypElement);
-    //     newElement.appendChild(bezeichnungElement);
-    //
-    //     // Appending the newElement to the document body (you can append it wherever you want in your HTML)
-    //     document.getElementById("deutsch-content").appendChild(newElement);
-    // }
-
 });
 
 document.getElementById('mathematik-link').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default link behavior
-
-    currentSelection = "mathematik";
-
+    currentSelection = 1;
     removeAll();
     document.getElementById("mathematik").style.display = "block";
-
-    // const elements = lehrplan_21.filter((item) => item.f_id === 1 && item.f_id !== null);
-    //
-    // for (const item of elements) {
-    //     let newElement = document.createElement('div');
-    //     newElement.classList.add('card'); // Adding a class for styling (you can customize this class in your CSS)
-    //
-    //     // Creating elements for subject, category, title, and description
-    //     let strukturtypElement = document.createElement('h2');
-    //     strukturtypElement.textContent = item.strukturtyp;
-    //     let bezeichnungElement = document.createElement('p');
-    //     bezeichnungElement.textContent = `Bezeichnung: ${item.bezeichnung}`;
-    //
-    //     // Appending elements to the newElement
-    //     newElement.appendChild(strukturtypElement);
-    //     newElement.appendChild(bezeichnungElement);
-    //
-    //     // Appending the newElement to the document body (you can append it wherever you want in your HTML)
-    //     document.getElementById("mathematik-content").appendChild(newElement);
-    // }
-
 });
 
 document.getElementById('natur-link').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default link behavior
-
-    currentSelection = "natur";
-
+    currentSelection = 2;
     removeAll();
     document.getElementById("natur").style.display = "block";
-
-    // const elements = lehrplan_21.filter((item) => item.f_id === 2 && item.f_id !== null);
-    //
-    // for (const item of elements) {
-    //     let newElement = document.createElement('div');
-    //     newElement.classList.add('card'); // Adding a class for styling (you can customize this class in your CSS)
-    //
-    //     // Creating elements for subject, category, title, and description
-    //     let strukturtypElement = document.createElement('h2');
-    //     strukturtypElement.textContent = item.strukturtyp;
-    //     let bezeichnungElement = document.createElement('p');
-    //     bezeichnungElement.textContent = `Bezeichnung: ${item.bezeichnung}`;
-    //
-    //     // Appending elements to the newElement
-    //     newElement.appendChild(strukturtypElement);
-    //     newElement.appendChild(bezeichnungElement);
-    //
-    //     // Appending the newElement to the document body (you can append it wherever you want in your HTML)
-    //     document.getElementById("natur-content").appendChild(newElement);
-    // }
-
 });
 
 document.getElementById('mensch-link').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default link behavior
-
-    currentSelection = "mensch";
-
+    currentSelection = 3;
     removeAll();
     document.getElementById("mensch").style.display = "block";
-
-    // const elements = lehrplan_21.filter((item) => item.f_id === 3 && item.f_id !== null);
-    //
-    // for (const item of elements) {
-    //     let newElement = document.createElement('div');
-    //     newElement.classList.add('card'); // Adding a class for styling (you can customize this class in your CSS)
-    //
-    //     // Creating elements for subject, category, title, and description
-    //     let strukturtypElement = document.createElement('h2');
-    //     strukturtypElement.textContent = item.strukturtyp;
-    //     let bezeichnungElement = document.createElement('p');
-    //     bezeichnungElement.textContent = `Bezeichnung: ${item.bezeichnung}`;
-    //
-    //     // Appending elements to the newElement
-    //     newElement.appendChild(strukturtypElement);
-    //     newElement.appendChild(bezeichnungElement);
-    //
-    //     // Appending the newElement to the document body (you can append it wherever you want in your HTML)
-    //     document.getElementById("mensch-content").appendChild(newElement);
-    // }
-
 });
 
 document.getElementById('gesellschaft-link').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default link behavior
-
-    currentSelection = "gesellschaft";
-
+    currentSelection = 4;
     removeAll();
     document.getElementById("gesellschaft").style.display = "block";
-
-    // const elements = lehrplan_21.filter((item) => item.f_id === 4 && item.f_id !== null);
-    //
-    // for (const item of elements) {
-    //     let newElement = document.createElement('div');
-    //     newElement.classList.add('card'); // Adding a class for styling (you can customize this class in your CSS)
-    //
-    //     // Creating elements for subject, category, title, and description
-    //     let strukturtypElement = document.createElement('h2');
-    //     strukturtypElement.textContent = item.strukturtyp;
-    //     let bezeichnungElement = document.createElement('p');
-    //     bezeichnungElement.textContent = `Bezeichnung: ${item.bezeichnung}`;
-    //
-    //     // Appending elements to the newElement
-    //     newElement.appendChild(strukturtypElement);
-    //     newElement.appendChild(bezeichnungElement);
-    //
-    //     // Appending the newElement to the document body (you can append it wherever you want in your HTML)
-    //     document.getElementById("gesellschaft-content").appendChild(newElement);
-    // }
-
 });
 
 document.getElementById('gestalten-link').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default link behavior
-
-    currentSelection = "gestalten";
-
+    currentSelection = 5;
     removeAll();
     document.getElementById("gestalten").style.display = "block";
-
-    // const elements = lehrplan_21.filter((item) => item.f_id === 5 && item.f_id !== null);
-    //
-    // for (const item of elements) {
-    //     let newElement = document.createElement('div');
-    //     newElement.classList.add('card'); // Adding a class for styling (you can customize this class in your CSS)
-    //
-    //     // Creating elements for subject, category, title, and description
-    //     let strukturtypElement = document.createElement('h2');
-    //     strukturtypElement.textContent = item.strukturtyp;
-    //     let bezeichnungElement = document.createElement('p');
-    //     bezeichnungElement.textContent = `Bezeichnung: ${item.bezeichnung}`;
-    //
-    //     // Appending elements to the newElement
-    //     newElement.appendChild(strukturtypElement);
-    //     newElement.appendChild(bezeichnungElement);
-    //
-    //     // Appending the newElement to the document body (you can append it wherever you want in your HTML)
-    //     document.getElementById("gestalten-content").appendChild(newElement);
-    // }
-
 });
 
 document.getElementById('musik-link').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default link behavior
-
-    currentSelection = "musik";
-
+    currentSelection = 6;
     removeAll();
     document.getElementById("gesellschaft").style.display = "gesellschaft";
-
-    // const elements = lehrplan_21.filter((item) => item.f_id === 6 && item.f_id !== null);
-    //
-    // for (const item of elements) {
-    //     let newElement = document.createElement('div');
-    //     newElement.classList.add('card'); // Adding a class for styling (you can customize this class in your CSS)
-    //
-    //     // Creating elements for subject, category, title, and description
-    //     let strukturtypElement = document.createElement('h2');
-    //     strukturtypElement.textContent = item.strukturtyp;
-    //     let bezeichnungElement = document.createElement('p');
-    //     bezeichnungElement.textContent = `Bezeichnung: ${item.bezeichnung}`;
-    //
-    //     // Appending elements to the newElement
-    //     newElement.appendChild(strukturtypElement);
-    //     newElement.appendChild(bezeichnungElement);
-    //
-    //     // Appending the newElement to the document body (you can append it wherever you want in your HTML)
-    //     document.getElementById("musik-content").appendChild(newElement);
-    // }
-
 });
 
 document.getElementById('bewegung-link').addEventListener('click', function (event) {
     event.preventDefault(); // Prevent the default link behavior
-
-    currentSelection = "bewegung";
-
+    currentSelection = 7;
     removeAll();
     document.getElementById("bewegung").style.display = "block";
-
-    // const elements = lehrplan_21.filter((item) => item.f_id === 7 && item.f_id !== null);
-    //
-    // for (const item of elements) {
-    //     let newElement = document.createElement('div');
-    //     newElement.classList.add('card'); // Adding a class for styling (you can customize this class in your CSS)
-    //
-    //     // Creating elements for subject, category, title, and description
-    //     let strukturtypElement = document.createElement('h2');
-    //     strukturtypElement.textContent = item.strukturtyp;
-    //     let bezeichnungElement = document.createElement('p');
-    //     bezeichnungElement.textContent = `Bezeichnung: ${item.bezeichnung}`;
-    //
-    //     // Appending elements to the newElement
-    //     newElement.appendChild(strukturtypElement);
-    //     newElement.appendChild(bezeichnungElement);
-    //
-    //     // Appending the newElement to the document body (you can append it wherever you want in your HTML)
-    //     document.getElementById("bewegung-content").appendChild(newElement);
-    // }
-
 });
